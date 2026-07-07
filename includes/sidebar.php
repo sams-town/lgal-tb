324ff704581b481b89c39f49389c4c038bbcd029
sams-town <samstown31@gmail.com>
2026-07-07 11:10:53 +0700

    fix(sidebar): import functions.php to resolve undefined function fatal error on dashboard

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,6 +2,7 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+require_once __DIR__ . '/functions.php';
 $current_page = basename($_SERVER['PHP_SELF']);
 $page_param = isset($_GET['page']) ? $_GET['page'] : '';
 $type_param = isset($_GET['type']) ? $_GET['type'] : '';
